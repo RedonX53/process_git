e1f679e59e420f63106f97d8cde442c61eeb805e
RedonX53 <mian.muhammad.ahmad.53@gmail.com>
2022-02-02 19:32:22 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+class CreateUser {
+    constructor(firstName, lastName, email, age, address) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.age = age;
+      this.address = address;
+    }
+    about() {
+      return `${this.firstName} is ${this.age} years old.`;
+    }
+    is18() {
+      return this.age >= 18;
+    }
+    sing() {
+      return "sing sing sing ";
+    }
+  }
+  
+  
+  const user1 = new CreateUser('Muhammad', 'Ahmad', 'mian.muhammad.ahmad.53@gmail.com', 24, "House # address address address address");
+  
+  
+  for(let key in user1){
+    if(user1.hasOwnProperty(key)){
+        console.log(key);
+    }
+  
+  }
